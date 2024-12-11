8e06401cd4fe57ecfa0377c6675ecedb5dfab72b
412-ChrisP <chrispbacon542@gmail.com>
2024-12-10 21:20:55 -0500

    finished addLeaseContract in LeaseDao

--- src/main/java/com/yearup/dealership/db/LeaseDao.java
+++ src/main/java/com/yearup/dealership/db/LeaseDao.java
@@ -1,17 +1,35 @@
 package com.yearup.dealership.db;
 
 import com.yearup.dealership.models.LeaseContract;
-
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-public class LeaseDao {
+public class LeaseDao
+{
     private DataSource dataSource;
 
     public LeaseDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    public void addLeaseContract(LeaseContract leaseContract) {
-        // TODO: Implement the logic to add a lease contract
+    public void addLeaseContract(LeaseContract leaseContract)
+    {
+        String sql = "INSERT INTO lease_contracts (contractId, vehicleVin, leaseStart, leaseEnd, monthlyPayment) VALUES (?, ?, ?, ?, ?)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement stmt = connection.prepareStatement(sql))
+        {
+            stmt.setInt(1, leaseContract.getContractId());
+            stmt.setString(2, leaseContract.getVin());
+            stmt.setDate(3, java.sql.Date.valueOf(leaseContract.getLeaseStart()));
+            stmt.setDate(4, java.sql.Date.valueOf(leaseContract.getLeaseEnd()));
+            stmt.setDouble(5, leaseContract.getMonthlyPayment());
+            stmt.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
